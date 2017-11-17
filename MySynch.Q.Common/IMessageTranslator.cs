7240d77e7bfa3b8975c4344c44f2e0b2910b15ae
oduma <tavi_duma2002@yahoo.co.uk>
2017-11-17 21:26:36 +0000

    Changed the dependency

--- MySynch.Q.Common/IMessageTranslator.cs
+++
@@ -1,9 +0,0 @@
-﻿using MySynch.Q.Common.Contracts;
-
-namespace MySynch.Q.Common
-{
-    public interface IMessageTranslator
-    {
-        TransferMessage Translate(TransferMessage inMessage);
-    }
-}
